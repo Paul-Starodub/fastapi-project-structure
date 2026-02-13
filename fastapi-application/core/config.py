ec9c52407b17e42739ff4a15cdf288122ba956ca
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-02-13 11:53:12 +0200

    refactor config

--- fastapi-application/core/config.py
+++ fastapi-application/core/config.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 from pydantic import PostgresDsn
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class RunConfig(BaseModel):
@@ -24,6 +24,13 @@ class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
     db: DatabaseConfig
+
+    model_config = SettingsConfigDict(
+        env_file=(".env.template", ".env"),
+        case_sensitive=False,
+        env_nested_delimiter="__",
+        env_prefix="APP_CONFIG__",
+    )
 
 
 settings = Settings()
