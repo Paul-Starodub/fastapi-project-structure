7baf51ebbdb07e0ead009e08f038ddc41b0db73c
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-02-13 18:41:58 +0200

    add user schemas, CRUD operations, and API routes for users

--- fastapi-application/api/api_v1/crud/users.py
+++ fastapi-application/api/api_v1/crud/users.py
@@ -4,9 +4,18 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import User
+from core.schemas.user import UserCreate
 
 
 async def get_all_users(db: AsyncSession) -> Sequence[User]:
     stmt = select(User).order_by(User.id)
     result = await db.execute(stmt)
     return result.scalars().all()
+
+
+async def create_user(db: AsyncSession, user_create: UserCreate) -> User:
+    user = User(**user_create.model_dump())
+    db.add(user)
+    await db.commit()
+    # await db.refresh(user)
+    return user
--- fastapi-application/api/api_v1/users.py
+++ fastapi-application/api/api_v1/users.py
@@ -1,14 +1,30 @@
-from fastapi import APIRouter, Depends
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api.api_v1.crud.users import get_all_users
 from core.config import settings
 from core.models import db_helper
-from core.schemas.user import User
+from core.schemas.user import User, UserCreate
+from .crud import users as users_crud
 
 router = APIRouter(prefix=settings.api.v1.prefix, tags=["users"])
 
 
 @router.get("/", response_model=list[User])
-async def get_users(db: AsyncSession = Depends(db_helper.session_getter)):
-    return await get_all_users(db=db)
+async def get_users(
+    # db: AsyncSession = Depends(db_helper.session_getter),
+    db: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+):
+    return await users_crud.get_all_users(db=db)
+
+
+@router.post("/", response_model=User, status_code=status.HTTP_201_CREATED)
+async def create_user(
+    user_create: UserCreate,
+    db: Annotated[AsyncSession, Depends(db_helper.session_getter)],  # modern approach
+    # db: AsyncSession = Depends(
+    #     db_helper.session_getter,
+    # )
+):
+    return await users_crud.create_user(db=db, user_create=user_create)
