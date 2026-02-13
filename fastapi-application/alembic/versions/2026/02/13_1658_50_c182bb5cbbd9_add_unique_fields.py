070f1d03f7a251a791ecc832f5c57a12aaa2ffdb
Pavlo Starobub <starodub.pavlo@alsodev.md>
2026-02-13 17:01:27 +0200

    add convention for naming

--- fastapi-application/alembic/versions/2026/02/13_1658_50_c182bb5cbbd9_add_unique_fields.py
+++ fastapi-application/alembic/versions/2026/02/13_1658_50_c182bb5cbbd9_add_unique_fields.py
@@ -21,10 +21,10 @@ depends_on: Union[str, Sequence[str], None] = None
 def upgrade() -> None:
     op.create_table(
         "users",
+        sa.Column("id", sa.Integer(), nullable=False),
         sa.Column("username", sa.String(), nullable=False),
         sa.Column("foo", sa.Integer(), nullable=False),
         sa.Column("bar", sa.Integer(), nullable=False),
-        sa.Column("id", sa.Integer(), nullable=False),
         sa.PrimaryKeyConstraint("id", name=op.f("pk_users")),
         sa.UniqueConstraint("foo", "bar", name=op.f("uq_users_foo")),
         sa.UniqueConstraint("username", name=op.f("uq_users_username")),
